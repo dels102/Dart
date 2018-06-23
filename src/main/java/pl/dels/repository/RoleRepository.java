7715fa28f12ea17f8f0ce5e512d6143a3d217bb7
TS-T071\danelczykl <lukasz.dan91@gmail.com>
2018-06-23 12:31:41 +0200

    Add repositories for role and user

---
+++ src/main/java/pl/dels/repository/RoleRepository.java
@@ -0,0 +1,9 @@
+package pl.dels.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import pl.dels.model.Role;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+	Role findByRoleName(String roleName);
+}
---
+++ src/main/java/pl/dels/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package pl.dels.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import pl.dels.model.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+	
+	User findByUsername(String username);
+}
