e7cc91efd980914ee614410941bd18f46fa3f388
TS-T071\danelczykl <lukasz.dan91@gmail.com>
2018-06-24 11:54:47 +0200

    Inject userDetailsRepository object, add checkDuplicate method and update details method

--- src/main/java/pl/dels/service/UserService.java
+++ src/main/java/pl/dels/service/UserService.java
@@ -4,9 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import pl.dels.model.User;
+import pl.dels.model.UserDetails;
 import pl.dels.model.Role;
 import pl.dels.repository.UserRepository;
 import pl.dels.repository.RoleRepository;
+import pl.dels.repository.UserDetailsRepository;
 
 @Service
 public class UserService {
@@ -14,15 +16,21 @@ public class UserService {
 	private static final String DEFAULT_ROLE = "ROLE_USER";
 	private UserRepository userRepository;
 	private RoleRepository roleRepository;
+	private UserDetailsRepository userDetailsRepository;
 
 	@Autowired
 	public void setUserRepository(UserRepository userRepository) {
 		this.userRepository = userRepository;
 	}
-	
+
 	@Autowired
 	public void setRoleRepository(RoleRepository roleRepository) {
 		this.roleRepository = roleRepository;
+	}
+	
+	@Autowired
+	public void setUserDetailsRepository(UserDetailsRepository userDetailsRepository) {
+		this.userDetailsRepository = userDetailsRepository;
 	}
 
 	public void addWithDefaultRole(User user) {
@@ -31,4 +39,18 @@ public class UserService {
 		userRepository.save(user);
 	}
 	
+	public void updateIfUserHaveDetails (User user, UserDetails userDetails) {
+		userDetailsRepository.save(userDetails);
+		userRepository.save(user);
+	}
+
+	public boolean checkDuplicate(String username, String email) {
+		boolean result = true;	
+		User userWithDuplicateUsername = userRepository.findByUsername(username);
+		User userWithDuplicateEmail = userRepository.findByEmail(email);
+		if (userWithDuplicateEmail != null || userWithDuplicateUsername != null) {		
+			result = false;		
+		}
+		return result;
+	}
 }
