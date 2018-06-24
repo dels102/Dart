954c2354549ca05d464280c1e2df1cb71377ee52
TS-T071\danelczykl <lukasz.dan91@gmail.com>
2018-06-24 12:00:41 +0200

    Add inerface for user details

---
+++ src/main/java/pl/dels/repository/UserDetailsRepository.java
@@ -0,0 +1,9 @@
+package pl.dels.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import pl.dels.model.UserDetails;
+
+public interface UserDetailsRepository extends JpaRepository<UserDetails, Long>{
+
+}
