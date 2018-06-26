2f4c7f69c4b20caa62e604b7be8cd2cdcca31eb3
TS-T071\danelczykl <lukasz.dan91@gmail.com>
2018-06-26 15:49:42 +0200

    Correct formatting

--- src/main/java/pl/dels/security/SecurityConfig.java
+++ src/main/java/pl/dels/security/SecurityConfig.java
@@ -14,33 +14,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	public UserDetailsService customUserDetailsService() {
 		return new CustomUserDetailsService();
 	}
-	
+
 	@SuppressWarnings("deprecation")
 	@Bean
 	public static NoOpPasswordEncoder passwordEncoder() {
-	return (NoOpPasswordEncoder) NoOpPasswordEncoder.getInstance();
+		return (NoOpPasswordEncoder) NoOpPasswordEncoder.getInstance();
 	}
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests()
-		.antMatchers("/register", "/resources/**", "/", "/success", "/problem").permitAll()
-		.anyRequest()
-		.authenticated()
-		.and()
-		  .formLogin()
-          .loginPage("/login")
-              .permitAll()
-		.and()
-        .csrf().disable();
-          /*.loginProcessingUrl("/processlogin")
-              .permitAll()
-              .usernameParameter("user")
-              .passwordParameter("pass")
-          .and()
-      .logout()
-          .logoutUrl("/logmeout")
-              .logoutSuccessUrl("/")
-              .permitAll();*/
+		http.authorizeRequests().antMatchers("/register", "/resources/**", "/", "/success", "/problem").permitAll()
+				.anyRequest().authenticated().and().formLogin().loginPage("/login").permitAll().and().csrf().disable();
 	}
 }
