da3209b46aac76e0235e7e6aedefc0a0552acf4c
TS-T071\danelczykl <lukasz.dan91@gmail.com>
2018-06-27 19:09:41 +0200

    Add bcrypt encoder

--- src/main/java/pl/dels/security/SecurityConfig.java
+++ src/main/java/pl/dels/security/SecurityConfig.java
@@ -1,11 +1,15 @@
 package pl.dels.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -15,15 +19,31 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		return new CustomUserDetailsService();
 	}
 
-	@SuppressWarnings("deprecation")
+	@Autowired
+	public void configureGlobalSecurity(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(customUserDetailsService());
+		auth.authenticationProvider(authenticationProvider());
+	}
+
 	@Bean
-	public static NoOpPasswordEncoder passwordEncoder() {
-		return (NoOpPasswordEncoder) NoOpPasswordEncoder.getInstance();
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+	@Bean
+	public DaoAuthenticationProvider authenticationProvider() {
+		DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+		authenticationProvider.setUserDetailsService(customUserDetailsService());
+		authenticationProvider.setPasswordEncoder(passwordEncoder());
+		return authenticationProvider;
 	}
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().antMatchers("/register", "/resources/**", "/", "/success", "/problem").permitAll()
-				.anyRequest().authenticated().and().formLogin().loginPage("/login").permitAll().and().csrf().disable();
+		http.authorizeRequests()
+				.antMatchers("/register", "/resources/**", "/", "/success", "/problem", "/about", "/contact",
+						"/contactOk")
+				.permitAll().anyRequest().authenticated().and().formLogin().loginPage("/login").permitAll().and().csrf()
+				.disable();
 	}
 }
