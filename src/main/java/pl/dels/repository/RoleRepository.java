b0f2fc241d0c3ab65cab903dbe8bf34dc94ff048
TS-T071\danelczykl <lukasz.dan91@gmail.com>
2018-06-27 19:05:17 +0200

    Remove empty lines

--- src/main/java/pl/dels/repository/RoleRepository.java
+++ src/main/java/pl/dels/repository/RoleRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import pl.dels.model.Role;
 
 public interface RoleRepository extends JpaRepository<Role, Long> {
-	
+
 	Role findByRoleName(String roleName);
 }
--- src/main/java/pl/dels/repository/UserDetailsRepository.java
+++ src/main/java/pl/dels/repository/UserDetailsRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import pl.dels.model.UserDetails;
 
-public interface UserDetailsRepository extends JpaRepository<UserDetails, Long>{
+public interface UserDetailsRepository extends JpaRepository<UserDetails, Long> {
 
 }
