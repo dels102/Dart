09c2d12d3d57a0f5758f4f5d5717f4c73eef2ddd
TS-T071\danelczykl <lukasz.dan91@gmail.com>
2018-11-11 13:25:38 +0100

    Update files

---
+++ src/main/java/pl/danelczyk/DartApplication.java
@@ -0,0 +1,12 @@
+package pl.danelczyk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DartApplication.class, args);
+	}
+}
---
+++ target/classes/data.sql
@@ -0,0 +1,1 @@
+insert into role(role_name, description) VALUES ("ROLE_USER", "default role for user");
